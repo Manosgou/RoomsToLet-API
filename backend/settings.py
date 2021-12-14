f2b620818066a48f6ebb0bea88a519385285f476
Manos Gouvrikos <manolisgouvrikos@gmail.com>
2021-12-14 20:44:40 +0200

    Add static files

--- backend/settings.py
+++ backend/settings.py
@@ -127,12 +127,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'backend/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 print(STATIC_ROOT)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'backend/static/'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
