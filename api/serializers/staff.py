24896ea63298be584efec4fb56c508e0864b151b
Manos Gouvrikos <manolisgouvrikos@gmail.com>
2021-12-30 17:03:03 +0200

    Update serializers

--- api/serializers/staff.py
+++ api/serializers/staff.py
@@ -3,6 +3,8 @@ from api.models import Requests
 
 
 class RequestsSeriliazer(serializers.ModelSerializer):
+    house = serializers.StringRelatedField()
+
     class Meta:
         model = Requests
         fields = ('id', 'house', 'description', 'status')
