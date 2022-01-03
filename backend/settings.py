b72682d70e02142a7f7609c435fd84527706a480
Manos Gouvrikos <manolisgouvrikos@gmail.com>
2022-01-03 16:49:50 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -59,7 +59,8 @@ INSTALLED_APPS = [
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env.str('CLOUD_NAME'),
     'API_KEY': env.str('API_KEY'),
-    'API_SECRET': env.str('API_SECRET')
+    'API_SECRET': env.str('API_SECRET'),
+    'STATICFILES_MANIFEST_ROOT': BASE_DIR/'static'
 }
 
 REST_FRAMEWORK = {
