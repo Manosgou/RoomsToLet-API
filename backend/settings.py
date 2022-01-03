711d064b1816040a2c0fb802b07586162ce01b98
Manos Gouvrikos <manolisgouvrikos@gmail.com>
2022-01-03 16:42:52 +0200

    Fix

--- backend/settings.py
+++ backend/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 ROOT_URLCONF = 'backend.urls'
 
